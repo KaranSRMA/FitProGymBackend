8b6e56919c78635a185e6eec8c3182c317ea9f94
Karan SRMA <thekaransharma2004@gmail.com>
2026-02-24 12:35:09 +0530

    Added requirements.txt

--- run.py
+++ run.py
@@ -5,10 +5,10 @@ if __name__ == "__main__":
         "app.main:app",
         host="0.0.0.0",
         port=8000,
-        # reload=True,
-        # ssl_keyfile="192.168.29.209-key.pem",
-        # ssl_certfile="192.168.29.209.pem",
-        docs_url=None,
-        redoc_url=None,
-        openapi_url=None
+        reload=True,
+        ssl_keyfile="192.168.29.209-key.pem",
+        ssl_certfile="192.168.29.209.pem",
+        # docs_url=None,
+        # redoc_url=None,
+        # openapi_url=None
     )
